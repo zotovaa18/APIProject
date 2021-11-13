18ca602a016ac34c007527ec04a0761a4aa13945
zotovaa18 <zotova.a18@gmail.com>
2021-11-13 23:47:09 +0300

    Database

--- APIProject/settings.py
+++ APIProject/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 }
 
 
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
