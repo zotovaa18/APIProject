84321b06321f92ee214be59aa7dcf3e7d88b3286
Анастасия Зотова <avzotova_4@edu.hse.ru>
2022-01-24 16:43:35 +0300

    new version

--- makevideo/urls.py
+++ makevideo/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import VideoList, VideoDetails, VideoCommandsList, VideoCommandsDetails
 
 urlpatterns = [
-    path('makevideo/video/', VideoList.as_view()),
-    path('makevideo/video/<str:pk>/', VideoDetails.as_view()),
-    path('makevideo/videocommands/', VideoCommandsList.as_view()),
-    path('makevideo/videocommands/<str:pk>/', VideoCommandsDetails.as_view()),
+    path('api/makevideo/video/', VideoList.as_view()),
+    path('api/makevideo/video/<str:pk>/', VideoDetails.as_view()),
+    path('api/makevideo/videocommands/', VideoCommandsList.as_view()),
+    path('api/makevideo/videocommands/<str:pk>/', VideoCommandsDetails.as_view()),
 ]
