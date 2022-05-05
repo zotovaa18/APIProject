dc532db03424c337cab3ba2723c2c535e2e1d383
Анастасия Зотова <avzotova_4@edu.hse.ru>
2022-05-05 17:32:07 +0300

    new version 5.05

--- APIProject/urls.py
+++ APIProject/urls.py
@@ -55,6 +55,5 @@ urlpatterns = [
     #path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     #path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('auth/', include('authentication.urls', namespace='authentication')),
-]
-#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
